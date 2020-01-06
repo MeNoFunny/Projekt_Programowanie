0ad3155ea4880f834e28a87340537a48fcf80d10
MeNoFunny <wojciech.chojnacki1995@gmail.com>
2020-01-06 17:21:07 +0100

    TicTacToe

---
+++ Gra/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gra
+{
+    public partial class Gra : Form
+    {
+        bool turn = true; //jeżli prawda = ruch ma gracz X, jeżeli falsz = ruch ma gracz O
+        int turn_count = 0;
+
+        public Gra()
+        {
+            InitializeComponent();
+        }
+
+        private void oGrzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gra stworzona przez Marcina Blendowskiego oraz Wojciecha Chojnackiego", "O grze kółko i krzyżyk");
+        }
+
+        private void wyjścieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            bool check_winner = false;
+
+            //sprawdzanie poziomo
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                check_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                check_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                check_winner = true;
+
+            //sprawdzanie pionowo
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                check_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                check_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                check_winner = true;
+
+            //sprawdzanie diagonalne
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                check_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                check_winner = true;
+
+            if (check_winner)
+            {
+                disableButtons();
+
+                String winner = "";
+                if (turn)
+                {
+                    winner = "O";
+                    o_count.Text = (Int32.Parse(o_count.Text) + 1).ToString();
+                }
+                else
+                {
+                    winner = "X";
+                    x_count.Text = (Int32.Parse(x_count.Text) + 1).ToString();
+                }
+                MessageBox.Show("Gracz " + winner + " wygrał!");
+            }//end if
+            else
+            {
+                if (turn_count == 9)
+                {
+                    draw_count.Text = (Int32.Parse(draw_count.Text) + 1).ToString();
+                    MessageBox.Show("Remisik!");
+                }
+            }
+            
+        }//end checkForWinner
+
+        private void disableButtons()
+        {
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+                catch { }
+            }          
+        }
+
+        private void nowaGraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;//aktywuje buttony
+                    b.Text = "";//zmienia wartosc na pusta    
+                }
+                catch { }
+            }
+        }
+
+        private void button_enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                if (turn)
+                    b.Text = "X";
+                else
+                    b.Text = "O";
+            }//end if
+        }
+
+        private void button_leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.Text = "";
+            }//end if
+        }
+
+        private void Gra_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetStartystykiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x_count.Text = "0";
+            o_count.Text = "0";
+            draw_count.Text = "0";
+        }
+    }
+}
